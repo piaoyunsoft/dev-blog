d4d99a5882c6829b866261015f78cb6dc5d427b9
rainlesvio <rainlesvio@163.com>
2016-10-13 15:49:57 +0800

    添加aes加密解密方法.--20161013

---
+++ codes/AESCipher/AESCipher/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  AESCipher
+//
+//  Created by Welkin Xie on 8/13/16.
+//  Copyright © 2016 WelkinXie. All rights reserved.
+//
+//  https://github.com/WelkinXie/AESCipher-iOS
+//
+
+#import "ViewController.h"
+#import "AESCipher.h"
+
+@interface ViewController ()
+@end
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *plainText = @"IAmThePlainText";
+    NSString *key = @"16BytesLengthKey";
+    NSString *cipherText = [AESCipher encryptAES:plainText key:key];
+    NSLog(@"%@", cipherText);
+    NSString *decryptedText = [AESCipher decryptAES:cipherText key:key];
+    NSLog(@"%@", decryptedText);
+}
+
+@end
