8427237c72747dec8839f43f0771dbf00f2adb9d
wuxueying <rainlesviowuxy@gmail.com>
2016-11-25 20:52:39 +0800

    udpate inspire code

--- subscription/inspire/Inspire/Home/Controller/IndexController.class.php
+++ subscription/inspire/Inspire/Home/Controller/IndexController.class.php
@@ -3,22 +3,41 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $nonce = $_GET['nonce'];
-        $token = $_GET['weixin'];
         $timestamp = $_GET['timestamp'];
+        $nonce = $_GET['nonce'];
+        $token = 'weixin';
         $signature = $_GET['signature'];
-        $echostr = $_GET['echostr'];
-        $array = array();
-        $array = array($nonce,$timestamp,$token);
+        $array = array($timestamp,$nonce,$token);
         sort($array);
-        $str = sha1(implode($array));
-        if ($str == $signature) {
-        	echo $echostr;
+        $tmpstr = sha1(implode('', $array));
+        if ($tmpstr == $signature && $echostr) {
+        	echo $_GET['echostr'];
         	exit;
+        } else {
+        	$this->responseMsg();
         }
     }
 
-    public function show() {
-    	echo 'inspire';
+    public function responseMsg() {
+    	$postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+    	$postObj = simplexml_load_string($postArr);
+    	if (strtolower($postObj->MsgType) == 'event') {
+    		if (strtolower($postObj->Event) == 'subscribe') {
+    			$toUser = $postObj->FromUserName;
+    			$fromUser = $postObj->toUserName;
+    			$time = time();
+    			$msgType = 'text';
+    			$content = "welcome to follow my subscribe";
+    			$template = "<xml>
+				           <ToUserName><![CDATA[%s]]></ToUserName>
+				           <FromUserName><![CDATA[%s]]></FromUserName>
+				           <CreateTime>%s</CreateTime>
+				           <MsgType><![CDATA[%s]]></MsgType>
+				           <Content><![CDATA[%s]]></Content>
+				           </xml>";
+				$info = sprintf($template,$toUser,$fromUser,$time,$msgType,$content);
+				echo $info;
+    		}
+    	}
     }
 }
