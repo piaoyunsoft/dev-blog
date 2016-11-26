a5dae0b9f79a3b47f029dbf7e4988853fb4e2bc1
wuxueying <rainlesviowuxy@gmail.com>
2016-11-26 23:56:05 +0800

    添加群发以及群发模板的代码

--- subscription/inspire/Inspire/Home/Controller/IndexController.class.php
+++ subscription/inspire/Inspire/Home/Controller/IndexController.class.php
@@ -37,6 +37,23 @@ class IndexController extends Controller {
                 $Index->responseSubscribe($postObj,$arr);
     		}
     	}
+
+        if (strtolower($postObj->Event) == 'click') {
+            if (strtolower($postObj->EventKey) == 'item1') {
+                $content = 'menu1';
+            } else if (strtolower($postObj->EventKey) == 'songs') {
+                $content = 'songs';
+            }
+            $Index = D('Index');
+            $Index->responseText($postObj,$content);
+        }
+
+        if (strtolower($postObj->Event) == 'view') {
+            $content = "transfer url: ".$postObj->EventKey;
+            $Index = D('Index');
+            $Index->responseText($postObj,$content);
+        }
+
         if (strtolower($postObj->MsgType) == 'text' && 
             trim($postObj->Content) == 'tuwen') {
             $arr = array(
@@ -81,16 +98,6 @@ class IndexController extends Controller {
         }
     }
 
-    // function http_curl() {
-    //     $ch = curl_init();
-    //     $url = 'http://www.baidu.com';
-    //     curl_setopt($ch, CURLOPT_URL, $url);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //     $output = curl_exec($ch);
-    //     curl_close($ch);
-    //     var_dump($output);
-    // }
-
     function http_curl($url,$type='get',$res='json',$arr='') {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -102,25 +109,13 @@ class IndexController extends Controller {
         $output = curl_exec($ch);
         curl_close($ch);
         if ($res == 'json') {
-            return json_decode($output,true);
+            if (curl_errno($ch)) {
+                return curl_errno($ch);
+            } else {
+                return json_decode($output,true);
+            }
         }
     }
-
-    // function getWxAccessToken() {
-    //     $appid = 'wx2708c404b0c52465';
-    //     $appsecret = '4b9d327c6b2027ba8782faf6f05e80a8';
-    //     $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret."";
-    //     $ch = curl_init();
-    //     curl_setopt($ch, CURLOPT_URL, $url);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //     $res = curl_exec($ch);
-    //     curl_close($ch);
-
-    //     if (curl_errno($ch)) {
-    //         var_dump(curl_errno($ch));
-    //     }
-    //     echo json_encode(json_decode($res));
-    // }
 
     function getWxServerIp() {
         $accessToken = "Olv5_vD-j8z5G_UyFNEFjT-rn2hpzDiGAzYQw0EGUukqioqmFMCR7-8kIXvvBhjRbGOI9UAXjLG4mt-3BsBifIzQh_-qySsmqMlzhqi-5d1f-fgL2jo097_98Qv6bNyNMCAiAAASIG";
@@ -152,39 +147,96 @@ class IndexController extends Controller {
     }
 
     public function definedItem() {
-        echo $access_token = $this->getWxAccessToken(); 
-        $url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN';
+        $access_token = $this->getWxAccessToken(); 
+        $url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$access_token.'';
         echo '<br />';
         $postArr = array(
             'button'=>array(
                 array(
-                    'name'=>'主页',
+                    'name'=>urlencode('主页'),
                     'type'=>'click',
-                    'key'=>'item1',
+                    'key'=>'item1'
                 ),
                 array(
-                    'name'=>'娱乐',
+                    'name'=>urlencode('娱乐'),
                     'sub_button'=>array(
                         array(
                             'name'=>'song',
                             'type'=>'click',
-                            'key'=>'songs',
+                            'key'=>'songs'
                         ),
                         array(
                             'name'=>'film',
-                            'type'=>'click',
-                            'url'=>'http://www.baidu.com',
-                        ),
+                            'type'=>'view',
+                            'url'=>'http://www.baidu.com'
+                        )
                     ),
                 ),
                 array(
-                    'name'=>'我的',
+                    'name'=>urlencode('我的'),
                     'type'=>'view',
                     'url'=>"http://www.qq.com",
                 ),
             ),
         );
-        echo  $postJson = json_encode($postArr);
+        echo  $postJson = urldecode(json_encode($postArr));
+        $res = $this->http_curl($url,'post','json',$postJson);
+        var_dump($res);
+    }
+
+    function sendMsgAll() {
+        echo $access_token = $this->getWxAccessToken(); 
+        echo '<hr /><hr />';
+        $url = 'https://api.weixin.qq.com/cgi-bin/message/mass/preview?access_token='.$access_token.'';
+        // 1.text
+        // $array = array(
+        //     'touser'=>'oqiVYwlFONhwtmY6QiLA1wFu6Dmk',
+        //     'text'=>array('content'=>'inspire is very good'),
+        //     'msgtype'=>'text'
+        // );
+        // 2.pic
+        $array = array(
+            'touser'=>'oqiVYwlFONhwtmY6QiLA1wFu6Dmk',
+            'mpnews'=>array('media_id'=>'p6rm31nD0TFjqWicc0dtLeNLbuAib4tHTia2MmdTVZKDaIYAKNiaYHpFVDPceFAoic9dIv7kzGQljdlU9tDzp8hLYSg'),
+            'msgtype'=>'mpnews'
+        );
+        // $array = array(
+        //     'touser'=>'oqiVYwlFONhwtmY6QiLA1wFu6Dmk',
+        //     'text'=>array('content'=>'inspire is very good'),
+        //     'msgtype'=>'text'
+        // );
+        $postJson = json_encode($array);
+        var_dump($postJson);
+        echo '<hr /><hr />';
+        $res = $this->http_curl($url,'post','json',$postJson);
+        var_dump($res);
+    }
+
+    function uploadWxImage() {
+        $access_token = $this->getWxAccessToken(); 
+        $url = 'https://api.weixin.qq.com/cgi-bin/material/add_material?access_token='.$access_token.'';
+        $file = dirname(__FILE__).'/test.jpg';
+        $postArr = array('media'=>"@".$file);
+        $res = $this->http_curl($url,'post','json',json_encode($postArr));
+        var_dump($res);
+    }
+
+    function sendTemplateMsg() {
+        // 15 templates
+        // less 10w one day
+        $access_token = $this->getWxAccessToken();
+        $url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token='.$access_token.'';
+        $array = array(
+            'touser'=>'oqiVYwlFONhwtmY6QiLA1wFu6Dmk',
+            'template_id'=>'yKhld8H3snIElbd7Wx_Bf2JMpewNQyXk6Vpp4YxWZT4',
+            'url'=>'http://www.baidu.com',
+            'data'=>array(
+                'name'=>array('value'=>'hello','color'=>'#173177'),
+                'money'=>array('value'=>100,'color'=>'#173177'),
+                'time'=>array('value'=>date('Y-m-d H:i:s'),'color'=>'#173177'),
+            ),
+        );
+        $postJson = json_encode($array);
         $res = $this->http_curl($url,'post','json',$postJson);
         var_dump($res);
     }
