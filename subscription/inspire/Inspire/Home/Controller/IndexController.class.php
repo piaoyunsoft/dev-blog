0ad7cce66a05c5b0e100137ba120e5d4ca0f86a1
wuxueying <rainlesviowuxy@gmail.com>
2016-11-26 10:00:19 +0800

    update inspire code.---20161126 10:00

--- subscription/inspire/Inspire/Home/Controller/IndexController.class.php
+++ subscription/inspire/Inspire/Home/Controller/IndexController.class.php
@@ -23,23 +23,100 @@ class IndexController extends Controller {
     	$postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
         $tmpStr = $postArr;
     	$postObj = simplexml_load_string($postArr);
-    	if (strtolower($postObj->MsgType) == 'event') {
-    		if (strtolower($postObj->Event) == 'subscribe') {
-    			$toUser = $postObj->FromUserName;
-    			$fromUser = $postObj->ToUserName;
-    			$time = time();
-    			$msgType = 'text';
-    			$content = 'subscribe: '.$postObj->ToUserName.'<br>wechat user openid: '.$postObj->FromUserName.'<br>feedback msgType: '.$tmpStr;
-    			$template = "<xml>
-				           <ToUserName><![CDATA[%s]]></ToUserName>
-				           <FromUserName><![CDATA[%s]]></FromUserName>
-				           <CreateTime>%s</CreateTime>
-				           <MsgType><![CDATA[%s]]></MsgType>
-				           <Content><![CDATA[%s]]></Content>
-				           </xml>";
-				$info = sprintf($template,$toUser,$fromUser,$time,$msgType,$content);
-				echo $info;
-    		}
-    	}
+        // 1.first subscribe reponse
+    // 	if (strtolower($postObj->MsgType) == 'event') {
+    // 		if (strtolower($postObj->Event) == 'subscribe') {
+    // 			$toUser = $postObj->FromUserName;
+    // 			$fromUser = $postObj->ToUserName;
+    // 			$time = time();
+    // 			$msgType = 'text';
+    // 			$content = 'subscribe: '.$postObj->ToUserName.'<br>wechat user openid: '.$postObj->FromUserName.'<br>feedback msgType: '.$tmpStr;
+    // 			$template = "<xml>
+				//            <ToUserName><![CDATA[%s]]></ToUserName>
+				//            <FromUserName><![CDATA[%s]]></FromUserName>
+				//            <CreateTime>%s</CreateTime>
+				//            <MsgType><![CDATA[%s]]></MsgType>
+				//            <Content><![CDATA[%s]]></Content>
+				//            </xml>";
+				// $info = sprintf($template,$toUser,$fromUser,$time,$msgType,$content);
+				// echo $info;
+    // 		}
+    // 	}
+
+        // 2.text reponse
+        // if (strtolower($postObj->MsgType) == 'text') {
+        //     $toUser = $postObj->FromUserName;
+        //     $fromUser = $postObj->ToUserName;
+        //     $time = time();
+        //     $msgType = 'text';
+        //     switch (trim($postObj->Content)) {
+        //         case 1:
+        //             $content = 'input 1';  
+        //             break;
+        //         case 2:
+        //             $content = 'input 2';
+        //             break;
+        //         case 3:
+        //             $content = "<a href='http://www.baidu.com'>baidu</a> ";   
+        //             break;
+        //         default:
+        //             $content = 'input other';    
+        //     }
+        //     $template = "<xml>
+        //                <ToUserName><![CDATA[%s]]></ToUserName>
+        //                <FromUserName><![CDATA[%s]]></FromUserName>
+        //                <CreateTime>%s</CreateTime>
+        //                <MsgType><![CDATA[%s]]></MsgType>
+        //                <Content><![CDATA[%s]]></Content>
+        //                </xml>";
+        //     $info = sprintf($template,$toUser,$fromUser,$time,$msgType,$content);
+        //     echo $info;
+        // }
+
+        // 3.pic + text
+        if (strtolower($postObj->MsgType) == 'text') {
+            if (trim($postObj->Content) == 'tuwen') {
+                $toUser = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $arr = array(
+                    array(
+                        'title'=>'inspire',
+                        'description'=>"inspire is very good",
+                        'url'=>'http://www.baidu.com',
+                        'picUrl'=>'http://t-1.tuzhan.com/7bf5fd085229/c-2/l/2013/08/08/02/4fa5cd36e0184058b4372fd9b6cad45c.jpg'
+                    ),
+                    array(
+                        'title'=>'inspire2',
+                        'description'=>"inspire is very good2",
+                        'url'=>'http://www.baidu.com',
+                        'picUrl'=>'http://t-1.tuzhan.com/7bf5fd085229/c-2/l/2013/08/08/02/4fa5cd36e0184058b4372fd9b6cad45c.jpg'
+                    ),
+                    array(
+                        'title'=>'inspire2',
+                        'description'=>"inspire is very good2",
+                        'url'=>'http://www.baidu.com',
+                        'picUrl'=>'http://t-1.tuzhan.com/7bf5fd085229/c-2/l/2013/08/08/02/4fa5cd36e0184058b4372fd9b6cad45c.jpg'
+                    ),
+                ); 
+                $template = "<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <ArticleCount>".count($arr)."</ArticleCount>
+                            <Articles>";
+                foreach ($arr as $key => $value) {
+                    $template .= "<item>
+                                <Title><![CDATA[".$value['title']."]]></Title> 
+                                <Description><![CDATA[".$value['description']."]]></Description>
+                                <PicUrl><![CDATA[".$value['picUrl']."]]></PicUrl>
+                                <Url><![CDATA[".$value['url']."]]></Url>
+                                </item>";
+                }
+                $template .= "</Articles>
+                            </xml>";
+                echo sprintf($template,$toUser,$fromUser,time(),'news');
+            }
+        }
     }
 }
