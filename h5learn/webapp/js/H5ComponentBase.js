30a41cdeb43f36fe4cff31def12199b1aee8e169
rainlesvio <rainlesviowuxy@gmail.com>
2016-12-08 23:23:14 +0800

    async h5componentpoint code

--- h5learn/webapp/js/H5ComponentBase.js
+++ h5learn/webapp/js/H5ComponentBase.js
@@ -2,8 +2,8 @@ var H5ComponentBase = function(name, cfg) {
 	var cfg = cfg || {};
 	var id = ('h5_c_' + Math.random()).replace('.','_');
 
-	var cls = ' h5_component_name_' + name + ' h5_component_' + cfg.type;
-	var component = $('<div class="h5_component '+cls+'" id="'+id+'">');
+	var cls = ' h5_component_' + cfg.type;
+	var component = $('<div class="h5_component '+ cls + ' h5_component_name_' + name +'" id="'+id+'">');
 
 	cfg.text && component.text(cfg.text);
 	cfg.width && component.width(cfg.width/2);
--- h5learn/webapp/js/H5ComponentPoint.js
+++ h5learn/webapp/js/H5ComponentPoint.js
@@ -1,2 +1,22 @@
-/* 散点图表组件对象 */
+var H5ComponentPoint = function(name, cfg) {
+	var component = new H5ComponentBase(name, cfg);
 
+	var base = cfg.data[0][1];
+
+	$.each(cfg.data, function(idx, item) {
+		var point = $('<div class="point point_' + idx + '" >');
+		point.text(item[0] + '-' +  item[1]);
+
+		var per = (item[1]/base) * 100 + '%';
+
+		point.width(per).height(per);
+
+		if (item[2]) {
+			point.css('background-color', item[2]);
+		}
+
+		component.append(point);
+	});
+
+	return component;
+}
